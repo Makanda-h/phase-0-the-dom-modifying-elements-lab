e8db5de499f43d2214f3c75e01564843b3d4ad43
Hope <hopemwaura39@gmail.com>
2024-07-09 09:44:22 +0300

    modiying elements

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.querySelector('main#main');
+main.remove();
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory";
+
+const name = 'Hope Makanda';
+newHeader.textContent = `${name} is the champion`;
+const body = document.querySelector('body');
+body.appendChild(newHeader);
